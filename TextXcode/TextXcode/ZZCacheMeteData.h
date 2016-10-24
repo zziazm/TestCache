594ca2601e6c9cb15c6070581377bb33d5593750
zziazm <1310726454@qq.com>
2016-10-24 17:41:06 +0800

    update

---
+++ TextXcode/TextXcode/ZZCacheMeteData.h
@@ -0,0 +1,13 @@
+//
+//  ZZCacheMeteData.h
+//  TextXcode
+//
+//  Created by zm on 2016/10/24.
+//  Copyright © 2016年 zmMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZZCacheMeteData : NSObject<NSSecureCoding>
+@property (nonatomic, strong) NSDate * creationDate;
+@end
---
+++ TextXcode/TextXcode/ZZCacheMeteData.m
@@ -0,0 +1,13 @@
+//
+//  ZZCacheMeteData.m
+//  TextXcode
+//
+//  Created by zm on 2016/10/24.
+//  Copyright © 2016年 zmMac. All rights reserved.
+//
+
+#import "ZZCacheMeteData.h"
+
+@implementation ZZCacheMeteData
+
+@end
